f6099b8e1e6c8a84c42c526e3e5caf241b0d2a48
Azouriss <remi.desboyaux@gmail.com>
2024-03-20 10:00:09 +0100

    Commit One

---
+++ lib/dataBourse.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class dataBourse extends StatefulWidget {
+  const dataBourse({super.key});
+
+  @override
+  State<dataBourse> createState() => _dataBourseState();
+}
+
+class _dataBourseState extends State<dataBourse> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
